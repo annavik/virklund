095fd9adb5476fdd4860fed7881187ea89675c47
Anna Viklund <anna.viklund@daresay.co>
2022-05-06 22:30:38 +0200

    Setup navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.scss";
 import { Intro } from "./components/Intro/Intro";
+import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className={styles.App}>
-      <div className={styles.Menu}></div>
+      <div className={styles.Menu}>
+        <NavBar />
+      </div>
       <div className={styles.ContentContainer}>
         <div className={styles.Content}>
-          <Intro />
+          <Routes>
+            <Route path="/" element={<Intro />} />
+            <Route path="/monster" element={<></>} />
+            <Route path="/virkskolan" element={<></>} />
+            <Route path="/kontakt" element={<></>} />
+          </Routes>
         </div>
       </div>
       <div className={styles.Footer}></div>
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,48 @@
+import classNames from "classnames";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Spacer } from "../Spacer/Spacer";
+import styles from "./NavBar.module.scss";
+import virklund from "./virklund.jpg";
+
+const navItems = [
+  { label: "Hem", path: "/" },
+  { label: "Mönster", path: "/monster" },
+  { label: "Virkskolan", path: "/virkskolan" },
+  { label: "Kontakt", path: "/kontakt" },
+];
+
+export const NavBar = () => {
+  return (
+    <div className={styles.NavBar}>
+      <a href="/">
+        <div className={styles.Logo}>
+          <img src={virklund}></img>
+          <Spacer size={10} />
+          <span>Anna Virklund</span>
+        </div>
+      </a>
+      <nav className={styles.NavItems}>
+        {navItems.map((navItem) => (
+          <NavItem navItem={navItem} />
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+const NavItem = ({ navItem }: { navItem: { label: string; path: string } }) => {
+  const resolvedPath = useResolvedPath(navItem.path);
+  const match = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <Link key={navItem.path} to={navItem.path} className={styles.NavItem}>
+      <span>{navItem.label}</span>
+      <div
+        className={classNames({
+          [styles.NavItemMarker]: true,
+          [styles.Active]: !!match,
+        })}
+      ></div>
+    </Link>
+  );
+};
