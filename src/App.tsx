dff16fc679d96543aa87c452fd20c4672d268c9d
Anna Viklund <anna.viklund@daresay.co>
2022-05-14 23:46:10 +0200

    Fix scroll issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import styles from "./App.module.scss";
 import { Footer } from "./components/Footer/Footer";
 import { NavBar } from "./components/NavBar/NavBar";
@@ -9,12 +10,20 @@ import { PatternPage } from "./pages/PatternPage/PatternPage";
 import { PatternsPage } from "./pages/PatternsPage/PatternsPage";
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    containerRef.current?.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className={styles.App}>
       <div className={styles.NavBarContainer}>
         <NavBar />
       </div>
-      <div className={styles.ContentContainer}>
+      <div className={styles.ContentContainer} ref={containerRef}>
         <div className={styles.Content}>
           <Routes>
             <Route path="/" element={<Home />} />
