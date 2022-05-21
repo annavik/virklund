f5e1cc77862627f84a38aabc22c6947cd90afbfe
Anna Viklund <anna.viklund@daresay.co>
2022-05-22 00:48:44 +0200

    Update port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -9,4 +10,4 @@ app.get("/*", function (_req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(9000);
+app.listen(port);
